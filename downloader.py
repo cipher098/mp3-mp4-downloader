77a077ea2a6491b4f17cd41a3f1a2ff2dee887f6
Saurabh Gothieval <saurabh@eventgraphia.in>
2017-08-17 15:12:24 +0530

    Add Youtube link scrapper

---
+++ downloader.py
@@ -0,0 +1,25 @@
+import re, urllib
+import pandas as pd
+from bs4 import BeautifulSoup
+
+query = "tujko+jo+paaya+original+hd+youtube"
+
+def get_youtube_link(query):
+	site = urllib.request.urlopen("http://duckduckgo.com/html/?q="+query)
+	data = site.read()
+	soup = BeautifulSoup(data, "html.parser")
+
+	my_list = soup.find("div", {"id": "links"}).find_all("div", {'class': re.compile('.*web-result*.')})[0:15]
+
+	(result__snippet, result_url) = (list() for i in range(2))
+
+	for i in my_list:         
+		try:
+		    result_url.append(i.find("a", {"class": "result__url"}).get_text().strip("\n").strip())
+		except:
+		    result_url.append(None)
+		if result_url:
+			break
+
+	# import ipdb; ipdb.set_trace()
+	return ("https://" + result_url[0])
